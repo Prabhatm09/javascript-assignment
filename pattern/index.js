8cc5b9d800333b29b37e7efcdc5117f0890834b7
prabhatm09 <prabhatmishra1409@gmail.com>
2022-07-01 20:41:43 +0530

    patterm

--- pattern/index.js
+++ pattern/index.js
@@ -1,4 +1,4 @@
-//  q-2
+// //  q-2
 
 let num = 5;
 let count = 1
@@ -13,7 +13,7 @@ for(let i=1; i<=num; i++){
 }
 
 
-// q-3
+// // q-3
 
 let a = 0;
 let b = 1
@@ -28,4 +28,37 @@ for(let i=1; i<=5; i++){
     
   }
   console.log(`  ${stars}`)
+}
+
+
+//  q-4
+for(let i=1;i<=3;i++){
+    let stars=" "
+    for(let space=1;space<=3-i;space++){
+        stars=stars+" "
+    }
+    let k=i
+    for(j=1;j<=i;j++){
+        stars=stars+k++
+    }
+     k=k-2;
+    for(let j=1;j<i;j++){
+        stars=stars+k--
+    }
+    console.log(stars)
+}
+for(let i=3-1;i>=1;i--){
+    let stars=" "
+    for(let space=1;space<=3-i;space++){
+        stars=stars+" "
+    }
+    let k=i
+    for(j=1;j<=i;j++){
+        stars=stars+k++
+    }
+     k=k-2;
+    for(let j=1;j<i;j++){
+        stars=stars+k--
+    }
+    console.log(stars)
 }
--- pattern/readme.md
+++ pattern/readme.md
@@ -1,2 +1,3 @@
 <img src="./pattern/2.png">
 <img src="./pattern/3.png">
+<img src="./pattern/4.png">
