b9c41fb2e89810df22c9466a615194c2fdb0328c
prabhatm09 <prabhatmishra1409@gmail.com>
2022-07-07 22:34:34 +0530

    function

--- pattern/index.js
+++ pattern/index.js
@@ -3,62 +3,61 @@
 let num = 5;
 let count = 1
 
-for(let i=1; i<=num; i++){
+for(let i=5; i>=1; i++){
   let stars = ''
-  for(let j=1; j<=i; j++){
-    stars = stars + ` ${count++}`
-    // count = count+1
-  }
+  for(let j=i; j<=num; j++){
+    stars = stars + j
+    }
   console.log(stars)
 }
 
 
 // // q-3
 
-let a = 0;
-let b = 1
+// let a = 0;
+// let b = 1
 
-for(let i=1; i<=5; i++){
-  let stars = ''
-  for(let j=1; j<=i; j++){
-    let c=a+b
-    a=b;
-    b=c
-    stars = stars + c
+// for(let i=1; i<=5; i++){
+//   let stars = ''
+//   for(let j=1; j<=i; j++){
+//     let c=a+b
+//     a=b;
+//     b=c
+//     stars = stars + c
     
-  }
-  console.log(`  ${stars}`)
-}
+//   }
+//   console.log(`  ${stars}`)
+// }
 
 
 //  q-4
-for(let i=1;i<=3;i++){
-    let stars=" "
-    for(let space=1;space<=3-i;space++){
-        stars=stars+" "
-    }
-    let k=i
-    for(j=1;j<=i;j++){
-        stars=stars+k++
-    }
-     k=k-2;
-    for(let j=1;j<i;j++){
-        stars=stars+k--
-    }
-    console.log(stars)
-}
-for(let i=3-1;i>=1;i--){
-    let stars=" "
-    for(let space=1;space<=3-i;space++){
-        stars=stars+" "
-    }
-    let k=i
-    for(j=1;j<=i;j++){
-        stars=stars+k++
-    }
-     k=k-2;
-    for(let j=1;j<i;j++){
-        stars=stars+k--
-    }
-    console.log(stars)
-}
+// for(let i=1;i<=3;i++){
+//     let stars=" "
+//     for(let space=1;space<=3-i;space++){
+//         stars=stars+" "
+//     }
+//     let k=i
+//     for(j=1;j<=i;j++){
+//         stars=stars+k++
+//     }
+//      k=k-2;
+//     for(let j=1;j<i;j++){
+//         stars=stars+k--
+//     }
+//     console.log(stars)
+// }
+// for(let i=3-1;i>=1;i--){
+//     let stars=" "
+//     for(let space=1;space<=3-i;space++){
+//         stars=stars+" "
+//     }
+//     let k=i
+//     for(j=1;j<=i;j++){
+//         stars=stars+k++
+//     }
+//      k=k-2;
+//     for(let j=1;j<i;j++){
+//         stars=stars+k--
+//     }
+//     console.log(stars)
+// }
--- pattern/readme.md
+++
@@ -1,3 +0,0 @@
-<img src="./pattern/2.png">
-<img src="./pattern/3.png">
-<img src="./pattern/4.png">
